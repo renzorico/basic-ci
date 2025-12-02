ca6b886d76456ae0a2d0db633c08a7bb0a1ba4d5
Renzo Rico <renzorico10@gmail.com>
2025-12-02 20:55:35 +0000

    induced an error

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -4,5 +4,4 @@ import unittest
 
 class TestSample(unittest.TestCase):
     def test_sample(self):
-        # We are simply checking whether 42==42!
-        self.assertEqual(42, 42)
+        self.assertEqual(42, 429)
